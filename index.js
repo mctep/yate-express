b4ccba4aa2b6a8ae73ee896ca1a700aa70a6e5fe
mctep <mctep@yandex-team.ru>
2012-09-19 04:01:37 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var yatePath = __dirname + '/node_modules/yate/lib/';
+
+var yate = require(yatePath + 'yate.js');
+require(yatePath + 'actions.js');
+
+exports.__express = function(path, options, fn) {
+	var key = path + ':string';
+
+	if ('function' == typeof options) {
+		fn = options, options = {};
+	}
+
+	try {
+		fn(null, yate.run(path, { data: options }));
+	} catch (err) {
+		fn(err);
+	}
+}
