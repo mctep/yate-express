2acf2e1fd2e8f19be1ab62f61bcb41f1af7446d7
mctep <mctep@yandex-team.ru>
2012-10-04 03:04:41 +0400

    add cahing

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
+var yate = require('yate');
 var fs = require('fs');
-var yatePath = __dirname + '/node_modules/yate/lib/';
+//var runtime = require('yate/lib/runtime')
 
-var yate = require(yatePath + 'yate.js');
-require(yatePath + 'actions.js');
+var settings = {
+	'caching': false
+};
+
+exports.set = function(setting, val) {
+	if (1 == arguments.length) {
+		if (settings.hasOwnProperty(setting)) {
+			return settings[setting];
+		}
+	} else {
+		settings[setting] = val;
+		return this;
+	}
+	return exports;
+};
 
 exports.__express = function(path, options, fn) {
+
+	if (!settings.caching || (settings.caching && !this.yateFn)) {
+		var runtimeJS = fs.readFileSync(__dirname + '/node_modules/yate/lib/runtime.js');
+		var compiledJS = yate.compile(path).js;
+
+		this.yateFn = this.yateFn || function(data) {
+			var yateJS = [
+				runtimeJS,
+				compiledJS,
+				'var data = ' + JSON.stringify(data),
+				'return yr.run("main", data, "")'
+			].join(';');
+			return new Function(yateJS)();
+		};
+	}
 
 	if ('function' == typeof options) {
 		fn = options, options = {};
 	}
 
 	try {
-		fn(null, yate.run(path, { data: options }));
+		fn(null, this.yateFn(options));
 	} catch (err) {
 		fn(err);
 	}
